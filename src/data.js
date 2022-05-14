97514a2e588fd7020bb76e3b40f7da4d62f6d23e
Kunal <kunal20vijay00@gmail.com>
2022-05-15 01:28:54 +0530

    Slider component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems =[
+    {
+        id:1,
+        img : "/img/slider1.png",
+        title : "SUMMER SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg:"#f5fafd"
+    },
+    {
+        id:2,
+        img : "/img/slider2.png",
+        title : "AUTUMN COLLECTION",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg:"#fcf1ed"
+    },
+    {
+        id:3,
+        img : "/img/slider3.png",
+        title : "LOUNGEWEAR LOVE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS",
+        bg:"#fbf0f4"
+    }
+]
