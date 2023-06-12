763b9b2e4eb86fc1b3ecdeaf1b445e18dcd98bb9
Kunal <kunal20vijay00@gmail.com>
2023-06-12 14:02:46 +0530

    added page routes using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
+  const user=true;
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={<ProductList/>}/>
+        <Route path="/products/:category" element={<ProductList/>}/>
+        <Route path="/product/:id" element={<Product/>}/>
+        <Route path="/register" element={user?<Navigate to="/" replace /> :<Register/>}/>
+        <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login />} />
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
+    </Router>
   );
 }
 
